8d8888507f7689b5a34e18649909af91688487b6
Rui Kumagai <kmri.nnnnn@gmail.com>
2022-05-02 16:41:54 +0900

    add: フロント環境&husky

---
+++ front/.storybook/main.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  typescript: { reactDocgen: false },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config, { configType }) => {
+    // Fixes npm packages that depend on some modules
+    config.resolve = {
+      extensions: ['.ts', '.tsx', '.js', '.css', '.scss'],
+      fallback: {
+        fs: false,
+        path: false,
+      },
+    };
+
+    // Sass & CSS Modules
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              auto: true,
+            },
+          },
+        },
+        'sass-loader',
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    // Important: return the modified config
+    return config;
+  },
+};
