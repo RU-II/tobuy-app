de1a7793c6bd5ace14c0bd0300ef389db3a86660
Rui Kumagai <kmri.nnnnn@gmail.com>
2022-05-04 01:21:34 +0900

    add: items-endpoint

--- api/server/repositories/items.repository.go
+++ api/server/repositories/items.repository.go
@@ -9,6 +9,10 @@ import (
 type ItemsRepository interface {
 	GetAllItems(item *[]models.Item, userId int) error
 	GetItemById(item *models.Item, id int, userId int) error
+	GetLastItem(item *models.Item, userId int) error
+	CreateItem(item *models.Item) error
+	DeleteItem(id int, userId int) error
+	UpdateItem(item *models.Item, id int, userId int) error
 }
 
 type itemsRepository struct {
@@ -35,5 +39,47 @@ func (ir *itemsRepository) GetItemById(item *models.Item, id int, userId int) er
 		return err
 	}
 
+	return nil
+}
+
+func (ir *itemsRepository) GetLastItem(item *models.Item, userId int) error {
+	if err := ir.db.Where("user_id=?", userId).Last(&item).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ir *itemsRepository) CreateItem(item *models.Item) error {
+	if err := ir.db.Create(&item).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ir *itemsRepository) DeleteItem(id int, userId int) error {
+	if err := ir.db.Where("id=? AND user_id=?", id, userId).Delete(&models.Item{}).Error; err != nil {
+		return err
+	}
+
+	// deleteすると必ず、RowsAffected < 1になるのでコメントアウト
+	// https://stackoverflow.com/questions/67154864/how-to-handle-gorm-error-at-delete-function
+	// if ir.db.Error != nil {
+	// 	return ir.db.Error
+	// } else {
+	// 	if ir.db.RowsAffected < 1 {
+	// 		// return nil
+	// 		return errors.Errorf("id=%w のアイテムデータが存在しません。", id)
+	// 	}
+	// }
+
+	return nil
+}
+
+func (ir *itemsRepository) UpdateItem(item *models.Item, id int, userId int) error {
+	if err := ir.db.Where("id=? AND user_id=?", id, userId).Updates(&item).Error; err != nil {
+		return err
+	}
 	return nil
 }
--- api/server/router/items.router.go
+++ api/server/router/items.router.go
@@ -21,6 +21,14 @@ func NewItemsRouter(ic controllers.ItemsController) ItemsRouter {
 
 func (ir *itemsRouter) SetItemsRouting(router *mux.Router) {
 	// router.HandleFunc(basePath+"/items", ir.ic.ItemsPage).Methods("GET")
+	// FetchAllItems
 	router.HandleFunc(basePath+"/items", ir.ic.FetchAllItems).Methods(http.MethodGet)
+	// CreateItem
+	router.HandleFunc(basePath+"/items", ir.ic.CreateItem).Methods(http.MethodPost)
+	// FetchItemById
 	router.HandleFunc(basePath+"/items/{id}", ir.ic.FetchItemById).Methods(http.MethodGet)
+	// DeleteItem
+	router.HandleFunc(basePath+"/items/{id}/delete", ir.ic.DeleteItem).Methods(http.MethodPost)
+	// UpdateItem
+	router.HandleFunc(basePath+"/items/{id}/update", ir.ic.UpdateItem).Methods(http.MethodPost)
 }
